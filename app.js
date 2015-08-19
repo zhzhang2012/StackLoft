c250f57825392fc83e9f888077a0ae1bed300207
zhzhang2012 <zhzhangtony@gmail.com>
2015-08-19 15:09:30 +0800

    Initial commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
+var host = require('./routes/host');
 var cloud = require('./cloud');
 
 var app = express();
@@ -11,6 +12,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static('bower_components'));
 
 // 加载云代码方法
 app.use(cloud);
@@ -25,6 +27,7 @@ app.get('/', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+app.use('/host', host);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var APP_KEY = process.env.LC_APP_KEY;
 var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
+AV.setProduction(true);
 
 var app = require('./app');
 
@@ -12,5 +13,5 @@ var app = require('./app');
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 var server = app.listen(PORT, function () {
-  console.log('Node app is running, port:', PORT);
+    console.log('Node app is running, port:', PORT);
 });
